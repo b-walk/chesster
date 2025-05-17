f4a2e9d063d4e95c152d04a8205c90b4fc3ed169
Blake Walker <blameswalker@gmail.com>
2025-05-16 18:48:02 -0700

    Create accessors for Board objects' table attributes

--- lib/chesster.rb
+++ lib/chesster.rb
@@ -1,4 +1,7 @@
 # TODO:
 # - Add a description of the module
 # - Require necessary submodules
+
+require_relative 'chesster/board.rb'
+
 module Chesster; end
--- lib/chesster/board.rb
+++ lib/chesster/board.rb
@@ -1,13 +1,46 @@
 # TODO:
-# - Add a method `Board#[]`which accepts `rank` and `file` parameters and returns @table[rank][file]
 # - Add a method `Board#empty? which accepts `rank` and `file` parameters and returns `true` if the square is empty, and `false` otherwise
+
+# CLASSIC BOARD MODEL
+# 7 ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
+# 6 ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+# 5 . . . . . . . .
+# 4 . . . . . . . .
+# 3 . . . . . . . .
+# 2 . . . . . . . .
+# 1 ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+# 0 ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+# X 0 1 2 3 4 5 6 7
+# chess unicode characters are kinda hard to read... might implement color display in the future
 
 module Chesster
   class Board
-    attr_reader :table
-
-    def initialize ##accept parameters for variants?
+    def initialize # accept parameters for variants?
       @table = Array.new(8) { Array.new(8) }
     end
+
+    # TABLE ACCESSORS
+    # Return the piece at the specified coordinates
+    def piece_at(row, col)
+      table[row][col]
+    end
+
+    # Add a piece to the board at the specified coordinates
+    def add(piece, row, col)
+      table[row][col] = piece
+    end
+
+    # Remove a piece from the board at the specified coordinates
+    def remove(row, col)
+      table[row][col] = nil
+    end
+
+    def empty_at?(row, col)
+      table[row][col].nil?
+    end
+
+    private
+
+    attr_reader :table
   end
 end
